f3ec6011f735bb9d3a96333b176891ccd12b1075
Leapppppp <1725209246@qq.com>
2024-05-23 22:14:05 +0800

    test
    test

---
+++ greensea-ui/greensea-energy-back/src/api/sys/menu.ts
@@ -0,0 +1,15 @@
+import { defHttp } from '@/utils/http/axios';
+import { getMenuListResultModel } from './model/menuModel';
+
+//这个就是获取菜单的接口
+enum Api {
+  GetMenuList = '/getMenuList',
+}
+
+/**
+ * @description: Get user menu based on id
+ */
+
+export const getMenuList = () => {
+  return defHttp.get<getMenuListResultModel>({ url: Api.GetMenuList });
+};
